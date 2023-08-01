e95e711c9c9ef129cc45ecc531fe704fdec6217d
Varun Mathur <varunmathur1990@gmail.com>
2023-08-01 14:49:50 +0530

    Update cli_reddittnews.py

--- src/cli_reddittnews.py
+++ src/cli_reddittnews.py
@@ -7,9 +7,9 @@ https://www.reddit.com/prefs/apps/
 pip install praw fire utilmy
 
 
-export reddit_client=''
-export reddit_client_secret='meYGqgbj1ya1BDKLNtoXu8lIkTGAYg'
-export reddit_ua='daily'
+export reddit_client='i05gtu1-NrbJqCxHFHxW3g'
+export reddit_client_secret='Ak1qkvZdqnAfCCSK7rq9HcyBuaWoTQ'
+export reddit_ua='Bot'
 export reddit_subreddit='MachineLearning,OpenAI,ChatGPT,OpenAIDev,learnmachinelearning'
 
 
@@ -27,9 +27,9 @@ from utilmy import (date_now, pd_to_file, os_makedirs, log)
 def run(query:str='icml 2023 ', dirout:str="ztmp/", subreddits=None, reddit_limit=10, reddit_sort='top', verbose=1):
 
     ### from https://www.reddit.com/prefs/apps/
-    client_id     = os.environ.get("reddit_client_id", "brookm291" )
-    client_secret = os.environ.get("reddit_client_secret", "KUPvDuyRdVmARYmWDX2JeQ" )
-    user_agent    = os.environ.get("reddit_ua",  "daily")   ### Same from Secret page
+    client_id     = os.environ.get("reddit_client_id", "i05gtu1-NrbJqCxHFHxW3g" )
+    client_secret = os.environ.get("reddit_client_secret", "Ak1qkvZdqnAfCCSK7rq9HcyBuaWoTQ" )
+    user_agent    = os.environ.get("reddit_ua",  "Bot")   ### Same from Secret page
     reddit = praw.Reddit(client_id = client_id,#my client id
                      client_secret = client_secret,  #your client secret
                      user_agent    = user_agent #user agent name
